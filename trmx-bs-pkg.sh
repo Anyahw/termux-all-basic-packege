a9900caefc925b657af48c9e768c212970db763d
ADDY STARK <84610398+addy8328@users.noreply.github.com>
2021-11-27 17:26:44 +0530

    Create trmx-bs-pkg.sh

---
+++ trmx-bs-pkg.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+
+echo "Hello I,m ADDY_STARK thare are all basic tools for TERMUX.... "
+
+pkg update
+
+pkg upgrade  -y
+
+apt update 
+
+apt upgrade  -y
+
+pkg install git -y
+ 
+pkg install wget -y
+
+pkg install curl -y
+
+pkg install ruby -y
+
+pkg install php -y
+
+pkg install python -y
+ 
+pkg install python2 -y
+
+echo "THANKS...FOR...USEING...MY...SCRIPTE.... "
+
